91e487dd4ef4569614d4f5928f6a4d2a85158f28
Adrian <adrian.albahaca@gmail.com>
2025-04-21 10:58:16 +0000

    Header de pilas y colas, y arreglos dinamicos de enteros creados

---
+++ practica-3/arreglosenteros.h
@@ -0,0 +1,6 @@
+#ifndef __ARREGLOS_ENTEROS_H__
+#define __ARREGLOS_ENTEROS_H__
+
+
+
+#endif
---
+++ practica-3/pilas-colas.h
@@ -0,0 +1,10 @@
+#ifndef __PILAS_COLAS_H__
+#define __PILAS_COLAS_H__
+
+// Estructura de datos
+struct _Pila {
+    ArregloEnteros *arr;
+    int ultimo;
+};
+typedef struct _Pila = *Pila;
+#endif
