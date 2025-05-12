1e985193527e2179e1e2a1e1ed6b05e9274196a7
Adrian <adrian.albahaca@gmail.com>
2025-05-12 14:26:31 +0000

    Hecho hasta btree_copiar

--- practica-4/btree/btree.c
+++ practica-4/btree/btree.c
@@ -69,3 +69,71 @@ void btree_recorrer(BTree arbol, BTreeOrdenDeRecorrido orden, FuncionVisitante v
   }
   return;
 }
+
+int btree_nnodos(BTree arbol) {
+  // Retornar la cantidad de nodos en el arbol
+  // Caso base: Punto nulo del arbol
+  if(btree_empty(arbol)) {
+    return 0;
+  }
+
+  // Paso inductivo: Contar elemento del árbol
+  else {
+    int i = 0;
+    i += btree_nnodos(arbol->left);
+    i += btree_nnodos(arbol->right);
+    return i;
+  }
+}
+
+int btree_buscar(BTree arbol, int dato) {
+  // Caso base 1: Se consigue el dato
+  if(arbol->dato == dato) {
+    return 1;
+  }
+
+  // Caso base 2: Se llega a una hoja
+  else if(btree_empty(arbol)) {
+    return 0;
+  }
+
+  // Buscar a la izquierda
+  if(btree_buscar(arbol->left, dato)) {
+    return 1;
+  }
+
+  // Buscar a la derecha
+  if(btree_buscar(arbol->right, dato)) {
+    return 1;
+  }
+}
+
+BTree btree_copiar(BTree arbol) {
+  // Caso base: Nodo hoja
+  if(arbol->left == NULL && arbol->right == NULL) {
+    return btree_unir(arbol->dato, btree_crear(), btree_crear());
+  }
+
+  // Paso inductivo: Unir subarbol izquierdo y derecho
+  else {
+    BTree l = btree_copiar(arbol->left);
+    BTree r = btree_copiar(arbol->right);
+    return btree_unir(arbol->dato, l, r);
+  }
+}
+
+int btree_altura(BTree arbol) {
+  
+}
+
+int btree_nnodos_profundidad(BTree arbol, int prof) {
+  // Retorna la cant. de nodos en una cierta profundidad
+}
+
+int btree_profundidad(BTree arbol, int dato) {
+  // Retorna la profundidad de donde está el dato. Si no está, retorna -1
+}
+
+int btree_sumar(BTree arbol) {
+  // Suma todos los elementos de un arbol
+}
--- practica-4/btree/btree.h
+++ practica-4/btree/btree.h
@@ -35,7 +35,41 @@ BTree btree_unir(int dato, BTree left, BTree right);
 /**
  * Recorrido del arbol, utilizando la funcion pasada.
  */
-void btree_recorrer(BTree arbol, BTreeOrdenDeRecorrido orden,
-                    FuncionVisitante visit);
+void btree_recorrer(BTree arbol, BTreeOrdenDeRecorrido orden, FuncionVisitante visit);
+
+/**
+ * Cantidad de nodos en el arbol
+ */
+int btree_nnodos(BTree arbol);
+
+/**
+ * Buscar un elemento y retornar 1 si lo encuentra. Si no está, retornar 0
+ */
+int btree_buscar(BTree arbol, int dato);
+
+/**
+ * Copiar un arbol
+ */
+BTree btree_copiar(BTree arbol);
+
+/**
+ * Altura de un arbol
+ */
+int btree_altura(BTree arbol);
+
+/**
+ * Cantidad de nodos en una cierta profundidad
+ */
+int btree_nnodos_profundidad(BTree arbol, int prof);
+
+/**
+ * Profundidad de un dato. Si no está, retornar -1
+ */
+int btree_profundida(BTree arbol);
+
+/**
+ * Sumar todos los elementos
+ */
+int btree_sumar(BTree arbol);
 
 #endif /* __BTREE_H__ */
